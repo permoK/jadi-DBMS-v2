cff67c799f4a80248fdb1ca5b776d170fca16883
permoK <perminusk21@gmail.com>
2024-06-10 14:43:44 +0300

    add static path

--- jadiv2/jadi/settings.py
+++ jadiv2/jadi/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 STATICFILES_DIRS = [
