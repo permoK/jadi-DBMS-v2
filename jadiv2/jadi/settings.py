793e77598682b1e14319e10c9068595b9abe35af
permoK <perminusk21@gmail.com>
2024-06-10 15:09:41 +0300

    fix static path

--- jadiv2/jadi/settings.py
+++ jadiv2/jadi/settings.py
@@ -139,17 +139,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# # STATIC_URL = 'static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'core/static'),
+# ]
+
+STATIC_URL = '~/jadiv2/core/static/'
+STATIC_ROOT = '~/jadi-DBMS-v2/jadiv2/core/static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static'),
-]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
