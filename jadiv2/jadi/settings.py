0e72bee06953bc5f7b54935f2b1e3abc98c13fa1
permoK <perminusk21@gmail.com>
2024-06-11 02:42:33 +0300

    test static

--- jadiv2/jadi/settings.py
+++ jadiv2/jadi/settings.py
@@ -141,13 +141,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '~/jadiv2/core/static/'
+STATIC_ROOT = '~/jadi-DBMS-v2/jadiv2/core/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static'),
-]
+
 
 # STATIC_URL = 'jadiv2/core/static/'
 # STATIC_ROOT = 'jadi-DBMS-v2/jadiv2/core/static'
