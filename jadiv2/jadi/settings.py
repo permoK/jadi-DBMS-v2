e3dedf5137046edc4c9a39aa1d0372440c3f249f
permoK <perminusk21@gmail.com>
2024-06-14 00:59:05 +0300

    fix static

--- jadiv2/jadi/settings.py
+++ jadiv2/jadi/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,8 +144,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = 'jadiv2/core/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
